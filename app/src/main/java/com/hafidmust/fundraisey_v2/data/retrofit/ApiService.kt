bf55e8739ea6009411d882cc3dce250bad45fe55
hafidmust <hafidalimustaqim13@gmail.com>
2023-06-07 08:29:03 +0700

    implement retrofit in detail fragment

--- app/src/main/java/com/hafidmust/fundraisey_v2/data/retrofit/ApiService.kt
+++ app/src/main/java/com/hafidmust/fundraisey_v2/data/retrofit/ApiService.kt
@@ -1,12 +1,16 @@
 package com.hafidmust.fundraisey_v2.data.retrofit
 
 import com.hafidmust.fundraisey_v2.data.request.LoginRequest
+import com.hafidmust.fundraisey_v2.data.response.DetailLoanResponse
 import com.hafidmust.fundraisey_v2.data.response.LoanResponse
 import com.hafidmust.fundraisey_v2.data.response.LoginResponse
+import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
+
 
 interface ApiService {
     @POST("v1/login")
@@ -17,4 +21,11 @@ interface ApiService {
 
     @GET("v1/loan/all")
     fun getAllLoan() : Call<LoanResponse>
+
+    @GET("v1/load/detail/{id}")
+    fun detailLoanById(
+        @Path("id")
+        id : Int
+    ) : Call<DetailLoanResponse>
+
 }
--- app/src/main/java/com/hafidmust/fundraisey_v2/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/hafidmust/fundraisey_v2/ui/detail/DetailFragment.kt
@@ -1,71 +1,53 @@
 package com.hafidmust.fundraisey_v2.ui.detail
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.hafidmust.fundraisey_v2.R
+import androidx.lifecycle.ViewModelProvider
+import com.hafidmust.fundraisey_v2.data.response.DataDetail
+import com.hafidmust.fundraisey_v2.databinding.FragmentDetailBinding
 
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [DetailFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class DetailFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private var _binding: FragmentDetailBinding? = null
+    private val binding get() = _binding!!
+    private var id: Int? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        //get id
+        id = DetailFragmentArgs.fromBundle(arguments as Bundle).id
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(com.hafidmust.fundraisey_v2.R.layout.fragment_detail, container, false)
+        _binding = FragmentDetailBinding.inflate(inflater, container, false)
+        return binding.root
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-//        val navBar = activity?.findViewById<BottomNavigationView>(R.id.nav_view)
-//        navBar?.visibility = View.GONE
-        val dataId = DetailFragmentArgs.fromBundle(arguments as Bundle).id
-        Log.d("hello from detail", dataId.toString())
+        val viewmodel = ViewModelProvider(this)[DetailViewModel::class.java]
+        id?.let { viewmodel.getDetailById(it) }
+        viewmodel.dataDetail.observe(viewLifecycleOwner){
+            setupView(it)
+        }
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DetailFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DetailFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun setupView(dataDetail: DataDetail) {
+        binding.tvProject.text = dataDetail.name
     }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 }
